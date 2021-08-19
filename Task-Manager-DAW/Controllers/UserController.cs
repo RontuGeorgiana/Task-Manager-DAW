85fd540006d595940b28f9dd7eacd6b75bb27fe1
Georgiana Rontu <georgiana.rontu@s.unibuc.ro>
2021-08-19 23:45:46 +0300

    Add project files.

---
+++ Task-Manager-DAW/Controllers/UserController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task_Manager_DAW.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Task_Manager_DAW.Controllers
+{
+    public class UserController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude = "role, rights, Id_user")] User user)
+        {
+            bool Status = false;
+            string message = "";
+            //
+            // Model Validation 
+            if (ModelState.IsValid)
+            {
+
+                #region //Email is already Exist 
+                var isExist = IsEmailExist(user.email);
+                if (isExist)
+                {
+                    ModelState.AddModelError("EmailExist", "Email already exists");
+                    return View(user);
+                }
+                #endregion
+
+
+                #region  Password Hashing 
+                user.password = Crypto.Hash(user.password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword); //
+                #endregion
+
+                #region Defaults
+                user.role = "member";
+                user.rights = true;
+                #endregion
+
+                #region Save to Database
+                using (TaskManagerDatabaseEntities dc = new TaskManagerDatabaseEntities())
+                {
+                    Console.Write(user);
+
+                    dc.Users.Add(user);
+
+                    dc.SaveChanges();
+                  
+                    Status = true;
+                }
+                #endregion
+            }
+            else
+            {
+                message = "Invalid Request";
+            }
+
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return View(user);
+        }
+
+        [NonAction]
+        public bool IsEmailExist(string emailID)
+        {
+            using (TaskManagerDatabaseEntities dc = new TaskManagerDatabaseEntities())
+            {
+                var v = dc.Users.Where(a => a.email == emailID).FirstOrDefault();
+                return v != null;
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserLogin login, string ReturnUrl = "")
+        {
+            string message = "";
+            using (TaskManagerDatabaseEntities dc = new TaskManagerDatabaseEntities())
+            {
+                var v = dc.Users.Where(a => a.email == login.Email).FirstOrDefault();
+                if (v != null)
+                {
+                    if (string.Compare(Crypto.Hash(login.Password), v.password) == 0)
+                    {
+                        int timeout = login.RememberMe ? 525600 : 20; // 525600 min = 1 year
+                        var ticket = new FormsAuthenticationTicket(login.Email, login.RememberMe, timeout);
+                        string encrypted = FormsAuthentication.Encrypt(ticket);
+                        var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrypted);
+                        cookie.Expires = DateTime.Now.AddMinutes(timeout);
+                        cookie.HttpOnly = true;
+                        Response.Cookies.Add(cookie);
+
+
+                        if (Url.IsLocalUrl(ReturnUrl))
+                        {
+                            return Redirect(ReturnUrl);
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    else
+                    {
+                        message = "Invalid credential provided";
+                    }
+                }
+                else
+                {
+                    message = "Invalid credential provided";
+                }
+            }
+            ViewBag.Message = message;
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "User");
+        }
+
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Task-Manager-DAW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Task_Manager_DAW.MvcApplication" Language="C#" %>
